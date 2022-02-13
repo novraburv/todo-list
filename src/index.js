d38753f0505bd71415e127724684b5cb393a74e9
novraburv <78609440+novraburv@users.noreply.github.com>
2022-02-13 11:29:33 +0700

    create UI controller module and new project form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 
 // imported from npm
 import "normalize.css";
+import "material-icons/iconfont/outlined.css";
 
 // imported from local src
 import "./css/global.css";
+
+import { UI } from "./js/UI";
+
+UI.init();
---
+++ src/js/components/formProject.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import "../../css/form.css";
+import { UI } from '../UI';
+
+export const Form = () => {
+	const form = document.createElement("div");
+	form.classList.add("form", "form-create-project");
+
+	const input = document.createElement("input");
+	input.classList.add("form__input");
+	input.type = "text";
+	input.placeholder = "Project Name";
+
+
+	const submit = document.createElement("button");
+	submit.classList.add("btn", "btn-submit");
+	submit.innerHTML = '<i class="material-icons-outlined">done</i>';
+	submit.addEventListener("click", () => {
+		console.log(`get some functions to process this form ${Math.floor(Math.random() * 10)}`);
+		UI.closeProjectForm();
+	});
+	form.append(input, submit);
+
+	return form;
+};
